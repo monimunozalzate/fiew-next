a1be0c7a082425c2c856cc8ef4fbba1e3f747e9b
monimunozalzate <106264010+monimunozalzate@users.noreply.github.com>
2023-05-29 22:28:49 -0500

    adding firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  // apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
+  // authDomain: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  // projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  // storageBucket: NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  // appId: NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyB11jv1d-4mJFu8OImU70ZxeqSH64x3llc",
+  authDomain: "fire-base-19d9a.firebaseapp.com",
+  projectId: "fire-base-19d9a",
+  storageBucket: "fire-base-19d9a.appspot.com",
+  messagingSenderId: "582138774097",
+  appId: "1:582138774097:web:b589c8b74013756d3aae70"
+
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
